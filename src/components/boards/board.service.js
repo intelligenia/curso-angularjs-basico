e917df66880756915deeb1bfc27a1ec92f4d2316
Iván García García <ivan@intelligenia.com>
2017-05-22 17:03:38 +0200

    Implementación de más métdodos del servicio board

--- src/components/boards/board.service.js
+++ src/components/boards/board.service.js
@@ -11,12 +11,28 @@
 		var api = {
 			list: list,
 			get: get,
-			add: add
+			add: add,
+			flow_steps: {
+				list: listFS,
+				get: getFS,
+				add: addFS,
+				tasks: {
+					list: listTasks,
+					get: getTask,
+					add: addTask,
+				}
+			}
 		};
 
 		//////////////////////////////////////////////////////////////////////
 		// IMPLEMENTACIÓN
 
+		// TABLEROS
+
+		/**
+		 * Listado de tableros
+		 * @returns Una promesa con la respuesta HTTP. Si el estatus es 200 el listado de tableros
+         */
 		function list ( ) {
 			return $http({
 				url: '/api/project/',
@@ -28,6 +44,11 @@
 				} );
 		}
 
+		/**
+		 * Información sobre un tablero
+		 * @param boardId El identificador del tablero
+         * @returns Una promesa con la respuesta HTTP. Si el estatus es 200 la información del tablero
+         */
 		function get ( boardId ) {
 			return $http({
 				url: '/api/project/' + boardId + '/',
@@ -39,7 +60,70 @@
 				} );
 		}
 
-		function add ( ) {
+		/**
+		 * Añade un nuevo tablero
+		 * @param board Información sobre el tablero
+		 * @returns Una promesa con la respuesta HTTP. Si el estatus es 200 el nuevo tablero añadido
+		 */
+		function add ( board ) {
+			return $http({
+				url: '/api/project/' + boardId + '/',
+				method: 'POST',
+				data: board
+			})
+				.then ( function ( res ) {
+					console.log(res.data);
+					return res.data;
+				} );
+		}
+
+		///////////////////////
+		// FLOW STEPS
+
+		/**
+		 * Listado de columnas (flow-step) de un determinado proyecto
+		 * @param boardId El identificador del tablero
+		 * @returns Una promesa con la respuesta HTTP. Si el estatus es 200 el listado de columnas
+         */
+		function listFS(projectId){
+			return $http({
+				url: '/api/flow-step/',
+				method: 'GET'
+			})
+				.then ( function ( res ) {
+					console.log(res.data);
+					return res.data;
+				} );
+		}
+
+		/**
+		 * Obtiene los datos de una columna (flow-step)
+		 * @param flowstepId El identificador de la columna (flow-step)
+		 * @returns Una promesa con la respuesta HTTP. Si el estatus es 200 los datos de la columna
+         */
+		function getFS(flowstepId){
+
+		}
+
+		/**
+		 * Añade una nueva columna (flow-step) a un tablero (board)
+		 */
+		function addFS(boardId, flowstep){
+
+		}
+
+		//////////////////////
+		// TAREAS
+
+		function listTaks(){
+
+		}
+
+		function getTask(){
+
+		}
+
+		function addTask(){
 
 		}
 
