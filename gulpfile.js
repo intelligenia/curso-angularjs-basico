b44e49e45f0df3fd7fc5d462587e0c27c7cfdccd
Mario J. Barchéin Molina <mario@intelligenia.com>
2017-05-22 16:46:02 +0200

    proxy

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ gulp.task('webserver', function() {
     .pipe(webserver({
       port: 3000,
       livereload: true,
-      open: true
+      open: true,
+      proxies: [
+        {
+          source: '/api',
+          target: 'http://localhost:8000/api'
+		}
+      ]
     }));
 });
